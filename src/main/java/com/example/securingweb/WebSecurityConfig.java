3f1a5b7abd242963845667acb69a5b3d7c3ec178
drmydrmy <vms05new@ya.ru>
2024-02-21 23:35:01 -0600

    Added a db config and a basic user manager

--- src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -1,16 +1,22 @@
 package com.example.securingweb;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -43,8 +49,25 @@ public class WebSecurityConfig {
 
 		return new InMemoryUserDetailsManager(user);
 	}
+
+	// userManager bean to create and read user data directly from db
+	// userManager.createUser(UserDetails user) adds user
+	// To build a user, use: UserDetails user = User.builder().username("user").password("encryptedpassword").roles("USER", "ADMIN").build()
+	@Bean
+	JdbcUserDetailsManager userManager(DataSource dataSource) throws Exception{
+		JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+		return users;
+	}
+
 	@Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+	// Allowing access to the h2 console (TURN OFF IN PRODUCTION)
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2/**"));
+}
+	
 }
